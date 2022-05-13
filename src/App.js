84a0e7ca6caa20ece8bed7bef8038b7f68730557
David Sneek <davidjfk@hotmail.com>
2022-05-13 14:52:31 +0200

    Part 1: Make a shopping list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import List from  './components/List';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <List />
     </div>
   );
 }
---
+++ src/components/List.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class List extends Component {
+  render() {
+    return (
+      <ul>
+        <li>apples</li>
+        <li>pumpkin</li>
+        <li>cabbage</li>
+      </ul>
+    )
+  }
+}
+
+export default List
